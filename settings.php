ac83b114b916ff872f74ba8eda22ca1e9f9951ac
Hittesh Ahuja <ha386@bath.ac.uk>
2016-08-26 14:03:22 +0100

    Line exceeds 132 characters; contains 150 characters

--- settings.php
+++ settings.php
@@ -30,7 +30,8 @@ if ($ADMIN->fulltree) {
         get_string('https_message_label', 'filter_bath_https_monitor'),
         get_string('https_message_desc', 'filter_bath_https_monitor'), 'Make sure embedded content on this site is HTTPS'));
     // Types.
-    $types = ['warning' => 'warning', 'info' => 'info', 'danger' => 'danger', 'default' => 'default', 'primary' => 'primary', 'success' => 'success'];
+    $types = ['warning' => 'warning', 'info' => 'info', 'danger' => 'danger',
+        'default' => 'default', 'primary' => 'primary', 'success' => 'success'];
     $settings->add(new admin_setting_configselect('filter_bath_https_monitor/https_message_type',
         get_string('https_message_type_label', 'filter_bath_https_monitor'),
         get_string('https_message_type_desc', 'filter_bath_https_monitor'), 'primary', $types));
