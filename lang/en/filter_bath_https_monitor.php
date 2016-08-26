a3a128b8fb5c356281431190eb8341c33d2f66ea
Hittesh Ahuja <ha386@bath.ac.uk>
2016-08-26 13:44:35 +0100

    Added license details

--- lang/en/filter_bath_https_monitor.php
+++ lang/en/filter_bath_https_monitor.php
@@ -1,4 +1,26 @@
 <?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+ *
+ * @package   filter_bath_https_monitor
+ * @copyright 2016 onwards University of Bath{@link http://moodle.bath.ac.uk}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $string['filtername'] = 'HTTPS Monitor';
 $string['https_message_label'] = 'Text message to replace with';
 $string['https_message_desc'] = 'Text message to replace with';
