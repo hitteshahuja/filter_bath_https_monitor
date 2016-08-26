73d56cda0e8fa673bcb93d1e4157fdfd285349bf
Hittesh Ahuja <ha386@bath.ac.uk>
2016-08-26 14:13:31 +0100

    Fixed using phpcbf

--- settings.php
+++ settings.php
@@ -26,17 +26,29 @@
 defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
     // Text message.
-    $settings->add(new admin_setting_confightmleditor('filter_bath_https_monitor/https_message',
-        get_string('https_message_label', 'filter_bath_https_monitor'),
-        get_string('https_message_desc', 'filter_bath_https_monitor'), 'Make sure embedded content on this site is HTTPS'));
+    $settings->add(
+        new admin_setting_confightmleditor(
+            'filter_bath_https_monitor/https_message',
+            get_string('https_message_label', 'filter_bath_https_monitor'),
+            get_string('https_message_desc', 'filter_bath_https_monitor'), 'Make sure embedded content on this site is HTTPS'
+        )
+    );
     // Types.
     $types = ['warning' => 'warning', 'info' => 'info', 'danger' => 'danger',
         'default' => 'default', 'primary' => 'primary', 'success' => 'success'];
-    $settings->add(new admin_setting_configselect('filter_bath_https_monitor/https_message_type',
-        get_string('https_message_type_label', 'filter_bath_https_monitor'),
-        get_string('https_message_type_desc', 'filter_bath_https_monitor'), 'primary', $types));
+    $settings->add(
+        new admin_setting_configselect(
+            'filter_bath_https_monitor/https_message_type',
+            get_string('https_message_type_label', 'filter_bath_https_monitor'),
+            get_string('https_message_type_desc', 'filter_bath_https_monitor'), 'primary', $types
+        )
+    );
     $positions = ['top', 'bottom'];
-    $settings->add(new admin_setting_configselect('filter_bath_https_monitor/https_message_position',
-        get_string('https_message_position_label', 'filter_bath_https_monitor'),
-        get_string('https_message_position_desc', 'filter_bath_https_monitor'), 'top', $positions));
+    $settings->add(
+        new admin_setting_configselect(
+            'filter_bath_https_monitor/https_message_position',
+            get_string('https_message_position_label', 'filter_bath_https_monitor'),
+            get_string('https_message_position_desc', 'filter_bath_https_monitor'), 'top', $positions
+        )
+    );
 }
