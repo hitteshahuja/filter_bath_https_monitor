20ba7849b74ddd8d9769c03c5fa3dad2ae7c02aa
Hittesh Ahuja <ha386@bath.ac.uk>
2016-08-26 13:50:36 +0100

    Added license details

--- filter.php
+++ filter.php
@@ -1,6 +1,29 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ *  Filter for warning users of non-https content on an https site
+ *
+ * @package    filter
+ * @subpackage bath_https_monitor
+ * @copyright  2016 Hittesh Ahuja (University of Bath) {@link moodle.bath.ac.uk}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
 /**
  * Class filter_bath_https_monitor
  */
