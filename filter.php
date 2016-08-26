72a1df67b802ccd0a2e330f51458b0353663f8ee
Hittesh Ahuja <ha386@bath.ac.uk>
2016-08-26 13:48:01 +0100

    Changed to please travis

--- filter.php
+++ filter.php
@@ -22,27 +22,27 @@ class filter_bath_https_monitor extends filter_mediaplugin
             return $text;
         }*/
         $message = get_config('filter_bath_https_monitor', 'https_message');
-        $bs_label = get_config('filter_bath_https_monitor', 'https_message_type');
+        $bsLabel = get_config('filter_bath_https_monitor', 'https_message_type');
         $position = get_config('filter_bath_https_monitor', 'https_message_position');
         if ($message == '') {
-            $https_warning_label = "<p class='label label-$bs_label'>" . self::MESSAGE . "</p>";
+            $httpsWarningLabel = "<p class='label label-$bsLabel'>" . self::MESSAGE . "</p>";
         } else {
-            $https_warning_label = "<p class='label label-$bs_label'>" . $message . "</p>";
+            $httpsWarningLabel = "<p class='label label-$bsLabel'>" . $message . "</p>";
         }
         $newtext = $text;
         if (!is_string($text) or empty($text)) {
-            // non string data can not be filtered anyway
+            // Non string data can not be filtered anyway.
             return $text;
         }
         $re1 = "~<iframe[^>]+>.*?</iframe>~i";
         $re2 = "~<embed[^>]+>.*?</embed>~i";
-        //Dont bother if the site is not https to begin with
+        // Dont bother if the site is not https to begin with.
         if (preg_match($re1, $text) || preg_match($re2, $text)) {
             if (stripos($text, "https://") === false) {
                 if ($position == 0) {
-                    $newtext = $https_warning_label . $text;
+                    $newtext = $httpsWarningLabel . $text;
                 } else {
-                    $newtext = $text . $https_warning_label;
+                    $newtext = $text . $httpsWarningLabel;
 
                 }
 
