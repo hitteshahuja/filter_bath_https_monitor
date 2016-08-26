5a12def2c6a0502b2460aee03c13fba3f51fe128
Hittesh Ahuja <ha386@bath.ac.uk>
2016-08-26 13:32:10 +0100

    Added language strings

--- lang/en/filter_bath_https_monitor.php
+++ lang/en/filter_bath_https_monitor.php
@@ -2,3 +2,8 @@
 $string['filtername'] = 'HTTPS Monitor';
 $string['https_message_label'] = 'Text message to replace with';
 $string['https_message_desc'] = 'Text message to replace with';
+$string['https_message_type_label'] = 'Message type';
+$string['https_message_type_desc'] = 'Boostrap CSS to apply';
+$string['https_message_label_default'] = 'Make sure embedded content on this site is HTTPS';
+$string['https_message_position_label'] = 'Position';
+$string['https_message_position_desc'] = 'Position of the message';
