46ca1940ad2ed9699b40b49be08578b835dc350f
Hittesh Ahuja <ha386@bath.ac.uk>
2016-08-26 14:02:55 +0100

    Inline comments must end in full-stops, exclamation marks, or question marks

--- settings.php
+++ settings.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 if ($ADMIN->fulltree) {
-    //Text message
+    // Text message.
     $settings->add(new admin_setting_confightmleditor('filter_bath_https_monitor/https_message',
         get_string('https_message_label', 'filter_bath_https_monitor'),
         get_string('https_message_desc', 'filter_bath_https_monitor'), 'Make sure embedded content on this site is HTTPS'));
-    //Types
+    // Types.
     $types = ['warning' => 'warning', 'info' => 'info', 'danger' => 'danger', 'default' => 'default', 'primary' => 'primary', 'success' => 'success'];
     $settings->add(new admin_setting_configselect('filter_bath_https_monitor/https_message_type',
         get_string('https_message_type_label', 'filter_bath_https_monitor'),
